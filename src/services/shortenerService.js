d070d83efb9177a3460be52a1ed1ef1327dce834
tarikly <tarikly+github@gmail.com>
2022-08-15 22:15:01 -0300

    fix URL_API env

--- src/services/shortenerService.js
+++ src/services/shortenerService.js
@@ -2,7 +2,7 @@ import baseAPI from "./api";
 
 class ShortenerService {
     constructor() {
-        this.api = baseAPI(process.env.URL_API);
+        this.api = baseAPI("http://url-shortener-api.tarikly.com/");
     }
 
     async getLink(code) {
